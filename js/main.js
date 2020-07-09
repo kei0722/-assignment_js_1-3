16e1aff5554fdeb302caf6980df95225506430a2
kei0722 <kayare919@gmail.com>
2020-07-09 11:53:20 +0900

    課題①-4のコードを追加

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 'use strict';
 
-const tasks = ['掃除','買い物','散歩'];
+const tasks = [
+  ['机を片付ける', '掃除'],
+  ['牛乳を買う', '買い物'],
+  ['散歩する', '散歩'],
+];
 
 function todoList() {
   console.log('=========================');
   console.log('現在持っているタスク一覧');
   console.log('=========================');
-  
-  tasks.forEach((task, index) => {
-    console.log(`${index}: ${task}`);
-  });
+
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i}：[内容]${tasks[i][0]}、[ジャンル]${tasks[i][1]}`);
+  }
 }
 
 todoList();
-
 const addTask = prompt('タスクを入力してください');
+const newTask= [];
+tasks.push(newTask);
+newTask[0] = addTask;
 
 if (addTask) {
-  alert('新しいタクスを追加しました。');
-  tasks.push(addTask);
-  todoList();
-  const input = prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
+  const addCategory = prompt('ジャンルを入力してください');
+
+  if (addCategory) {
+
+    alert('新しいタクスを追加しました。');
+    newTask[1] = addCategory;
+    todoList();
+    const input = prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
+  }
 }
